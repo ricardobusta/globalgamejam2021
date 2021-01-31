9a8dd895b88647667e4177bec4bc992b78ddcfd2
aki-cat <orenjiakira@gmail.com>
2021-01-31 14:48:57 -0300

    Fucking FOV

--- Assets/Scripts/2DBasics/FieldOfView.cs
+++ Assets/Scripts/2DBasics/FieldOfView.cs
@@ -1,89 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
 {
-    [SerializeField] private MeshFilter _meshFilter;
+    public const string PROPS_LAYER = "Props";
 
-    [SerializeField] private int _rayCount = 36;
-    [Range(0, 360)] [SerializeField] private float _fovAngle = 90f;
-    [SerializeField] private float _range = 5f;
-    [SerializeField] private LayerMask _layerMask;
-
-    private Mesh _mesh;
-
-    private Vector3 _origin;
-    private float _angle;
-
-    private void Start()
-    {
-        _mesh = new Mesh();
-        _meshFilter.mesh = _mesh;
-    }
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _followSpeed = 30f;
+    [SerializeField] private Material _litMaterial;
+    [SerializeField] private Material _diffuseMaterial;
 
     private void Update()
     {
-        SetOrigin(Vector3.zero);
-        GenerateMesh();
+        Vector2 movement = (_target.position - transform.position) * _followSpeed * Time.deltaTime;
+        transform.position += new Vector3(movement.x, movement.y);
     }
 
-    private void GenerateMesh()
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        float fovAngleRad = (Mathf.PI / 180f) * _fovAngle;
-        float angleStep = fovAngleRad / _rayCount;
+        var spriteRenderer = collider.GetComponent<SpriteRenderer>();
 
-        Vector3[] vertices = new Vector3[_rayCount + 2];
-        Vector2[] uv = new Vector2[vertices.Length];
-        int[] triangles = new int[_rayCount * 3 + 3];
-
-        vertices[0] = _origin;
-        uv[0] = Vector2.zero;
-
-        int vertexIndex = 1;
-        int triangleIndex = 0;
-        for (int i = 0; i <= _rayCount; i++)
+        if (spriteRenderer != null && spriteRenderer.sortingLayerName == PROPS_LAYER)
         {
-            float vertexAngle = _angle + (fovAngleRad * 0.5f) - angleStep * i;
-            Vector3 rayDirection = new Vector3(Mathf.Cos(vertexAngle), Mathf.Sin(vertexAngle));
-            Vector3 vertex = _origin + rayDirection * _range;
-
-            RaycastHit2D collision = Physics2D.Raycast(transform.position, rayDirection, _range, _layerMask);
-            if (collision.collider != null)
-            {
-                vertex = new Vector3(collision.point.x, collision.point.y) - transform.position;
-            }
-
-            vertices[vertexIndex] = vertex;
-            uv[vertexIndex] = new Vector2(vertex.x, vertex.y);
-
-            if (i > 0)
-            {
-                triangles[triangleIndex + 0] = 0;
-                triangles[triangleIndex + 1] = vertexIndex - 1;
-                triangles[triangleIndex + 2] = vertexIndex;
-            }
-
-            triangleIndex += 3;
-            vertexIndex++;
-        }
-
-        _mesh.vertices = vertices;
-        _mesh.uv = uv;
-        _mesh.triangles = triangles;
-    }
-
-    private void SetOrigin(Vector3 origin)
-    {
-        _origin = origin;
-    }
-
-    public void SetDirection(Vector3 direction)
-    {
-        direction = direction.normalized;
-        _angle = Mathf.Atan2(direction.y, direction.x);
-        if (_angle < 0)
-        {
-            _angle += Mathf.PI * 2;
+            spriteRenderer.material = _litMaterial;
         }
     }
 
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        var spriteRenderer = collider.GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null && spriteRenderer.sortingLayerName == PROPS_LAYER)
+        {
+            spriteRenderer.material = _diffuseMaterial;
+        }
+    }
 }
